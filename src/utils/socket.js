b2d8cc7e5d13c874e34ec0df8b1d1ed055e5e417
Ajay joseph <aj2506039@gmail.com>
2025-11-07 15:10:41 +0530

    Chat updated

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 // Socket.IO client instance
-export const socket = io("http://localhost:5000"); // Make sure this URL matches backend
+export const socket = io("https://project-nestify-backend.onrender.com"); // Make sure this URL matches backend
 
 // Room ID helper (centralized 🔁)
 export const getRoomId = (id1, id2) => [id1, id2].sort().join("_");
