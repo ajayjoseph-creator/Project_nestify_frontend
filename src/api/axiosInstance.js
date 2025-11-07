bd8914784840cb6b123dd09db0aaa7095ab293da
Ajay joseph <aj2506039@gmail.com>
2025-11-07 15:19:57 +0530

    feat: just updated

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -4,9 +4,9 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
-  withCredentials: true, 
+  withCredentials: true,
   headers: {
-    "Content-Type": "application/json",
+    "Content-Type": "application/json"
   },
 });
 
